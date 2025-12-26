1692450923a17442aafa06d2db7f50edbe3f17e6
Sheshbabu <sheshbabu@gmail.com>
2025-12-26 10:55:00 +0800

    Fixed progress spinner wobble

--- commons/components/Icon.jsx
+++ commons/components/Icon.jsx
@@ -61,7 +61,10 @@ export function TaskDoneIcon() {
 
 export function TaskInProgressIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-loader-circle-icon lucide-loader-circle"><path d="M21 12a9 9 0 1 1-6.219-8.56" /></svg>
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-loader-circle-icon lucide-loader-circle">
+      <circle cx="12" cy="12" r="10" opacity="0.25" />
+      <path d="M12 2 A10 10 0 0 1 22 12" />
+    </svg>
   );
 }
 
